18dc1f8a0acd6b860a2cff3b457a5103853dfcba
Sumin <new.sumyang@gmail.com>
2025-10-21 20:45:01 +0900

    feat: 애플리케이션 이름 입력기능 추가

--- src/main/java/racingcar/application/RacingCarApplication.java
+++ src/main/java/racingcar/application/RacingCarApplication.java
@@ -5,12 +5,15 @@ import racingcar.application.config.AppConfig;
 public class RacingCarApplication {
 
     public final Printer printer;
+    public final Reader reader;
 
     public RacingCarApplication(AppConfig appConfig) {
         printer = appConfig.printer();
+        reader = appConfig.reader();
     }
 
     public void run() {
         printer.printStartingMessage();
+        String carNamesInput = reader.read();
     }
 }
--- src/main/java/racingcar/application/config/AppConfig.java
+++ src/main/java/racingcar/application/config/AppConfig.java
@@ -1,11 +1,17 @@
 package racingcar.application.config;
 
 import racingcar.application.Printer;
+import racingcar.application.Reader;
 import racingcar.application.printer.RacingCarPrinter;
+import racingcar.application.reader.RacingCarReader;
 
 public class AppConfig {
 
     public Printer printer() {
         return new RacingCarPrinter();
+    }
+
+    public Reader reader() {
+        return new RacingCarReader();
     }
 }
