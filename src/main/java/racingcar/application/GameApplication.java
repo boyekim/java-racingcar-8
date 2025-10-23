67cb3c562015fb4fdf2610871c365d39f6ea7b0a
Sumin <new.sumyang@gmail.com>
2025-10-23 20:51:04 +0900

    feat(GameApplication): 전체 게임과 각 레이스 구별에 따른 게임 애플리케이션 추가

---
+++ src/main/java/racingcar/application/GameApplication.java
@@ -0,0 +1,40 @@
+package racingcar.application;
+
+import java.util.ArrayList;
+import racingcar.application.config.AppConfig;
+import racingcar.domain.Cars;
+import racingcar.domain.GameNumber;
+import racingcar.domain.RacingResult;
+import racingcar.service.RacingCarService;
+
+public class GameApplication {
+
+    private final Printer printer;
+    private final Reader reader;
+    private final ForwardStrategy forwardStrategy;
+    private final RacingCarService racingCarService;
+
+    public GameApplication(AppConfig appConfig) {
+        this.printer = appConfig.printer();
+        this.reader = appConfig.reader();
+        this.forwardStrategy = appConfig.forwardStrategy();
+        this.racingCarService = appConfig.racingCarService();
+    }
+
+    public void run() {
+        printer.printStartingMessage();
+        String carNamesInput = reader.read();
+        Cars cars = racingCarService.saveCars(carNamesInput);
+
+        printer.printCountMessage();
+        String racingCounts = reader.read();
+
+        RacingApplication racingApplication = new RacingApplication(forwardStrategy);
+        RacingResult afterRaceCars = racingApplication.race(
+                cars,
+                GameNumber.of(racingCounts).getValue(),
+                new ArrayList<>()
+        );
+        printer.printResult(afterRaceCars);
+    }
+}
