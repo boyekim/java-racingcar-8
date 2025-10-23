99726f82f3041aa1c7bf1d16a87b5e10087290e5
Sumin <new.sumyang@gmail.com>
2025-10-23 20:53:12 +0900

    feat(RacingResult): 게임 결과를 관리할 도메인 객체 추가

---
+++ src/main/java/racingcar/domain/RacingResult.java
@@ -0,0 +1,37 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class RacingResult {
+    List<Cars> value;
+
+    private RacingResult(List<Cars> value) {
+        this.value = value;
+    }
+
+    public static RacingResult of(List<Cars> result) {
+        return new RacingResult(result);
+    }
+
+    public List<String> findWinners() {
+        Cars lastResult = value.getLast();
+        List<Car> allCar = lastResult.getValue();
+        Integer winnerDistance = findWinnerDistance(allCar);
+        return lastResult.getValue().stream()
+                .filter(car -> winnerDistance.equals(car.getDistance()))
+                .map(Car::getName)
+                .toList();
+    }
+
+    public Integer findWinnerDistance(List<Car> cars) {
+        int result = 0;
+        for (Car car : cars) {
+            result = Math.max(result, car.getDistance());
+        }
+        return result;
+    }
+
+    public List<Cars> getValue() {
+        return value;
+    }
+}
