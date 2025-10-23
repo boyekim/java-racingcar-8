bc72105cfb2541ce8312e47f315da1ce51a0b2f8
Sumin <new.sumyang@gmail.com>
2025-10-23 20:53:42 +0900

    feat(RacingCarErrorCode): 게임 예외 메시지 상수 추가

--- src/main/java/racingcar/exception/RacingCarErrorCode.java
+++ src/main/java/racingcar/exception/RacingCarErrorCode.java
@@ -4,6 +4,7 @@ public enum RacingCarErrorCode {
     NAME_LENGTH_LIMIT_EXCEEDED("이름은 %s자를 초과할 수 없습니다."),
     NAME_REQUIRED("이름은 비어있을 수 없습니다."),
     INVALID_GAME_NUMBER("게임 횟수는 숫자가 입력되어야 합니다."),
+    GAME_NUMBER_REQUIRED("게임 횟수가 입력되어야 합니다.")
     ;
 
     private final String message;
