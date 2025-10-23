e67bc12edd127864f9b3f89a4c0cd5e294e8695a
Sumin <new.sumyang@gmail.com>
2025-10-23 20:55:02 +0900

    refactor(Application): 메서드명 변경에 따른 로직 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,12 @@
 package racingcar;
 
-import racingcar.application.RacingCarApplication;
+import racingcar.application.GameApplication;
 import racingcar.application.config.AppConfig;
 
 public class Application {
     public static void main(String[] args) {
         AppConfig appConfig = new AppConfig();
-        RacingCarApplication racingCarApplication = new RacingCarApplication(appConfig);
-        racingCarApplication.run();
+        GameApplication gameApplication = new GameApplication(appConfig);
+        gameApplication.run();
     }
 }
