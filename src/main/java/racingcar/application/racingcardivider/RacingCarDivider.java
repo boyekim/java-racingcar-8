ace28cb6afa935d0b03477ebcd840f90c2202cb4
Sumin <new.sumyang@gmail.com>
2025-10-25 01:34:40 +0900

    refactor(RacingCarDivider): 예외 케이스 정책 추가

--- src/main/java/racingcar/application/racingcardivider/RacingCarDivider.java
+++ src/main/java/racingcar/application/racingcardivider/RacingCarDivider.java
@@ -3,6 +3,8 @@ package racingcar.application.racingcardivider;
 import java.util.Arrays;
 import java.util.List;
 import racingcar.application.DivideStrategy;
+import racingcar.exception.RacingCarErrorCode;
+import racingcar.exception.RacingCarException;
 
 public class RacingCarDivider implements DivideStrategy {
 
@@ -10,6 +12,14 @@ public class RacingCarDivider implements DivideStrategy {
 
     @Override
     public List<String> divideByDelimiter(String input) {
-        return Arrays.stream(input.split(DELIMITER)).toList();
+        List<String> dividedNames = Arrays.stream(input.split(DELIMITER)).toList();
+        validateDuplicatedNames(dividedNames);
+        return dividedNames;
+    }
+
+    private void validateDuplicatedNames(List<String> dividedNames) {
+        if (dividedNames.size() != dividedNames.stream().distinct().count()) {
+            throw new RacingCarException(RacingCarErrorCode.DUPLICATED_NAME);
+        }
     }
 }
--- src/main/java/racingcar/exception/RacingCarErrorCode.java
+++ src/main/java/racingcar/exception/RacingCarErrorCode.java
@@ -6,6 +6,7 @@ public enum RacingCarErrorCode {
     INVALID_GAME_NUMBER("게임 횟수는 숫자가 입력되어야 합니다."),
     GAME_NUMBER_REQUIRED("게임 횟수가 입력되어야 합니다."),
     NAME_REQUIRED("최소 한명의 이름이 입력되어야 합니다."),
+    DUPLICATED_NAME("이미 존재하는 이름입니다."),
     ;
 
     private final String message;
