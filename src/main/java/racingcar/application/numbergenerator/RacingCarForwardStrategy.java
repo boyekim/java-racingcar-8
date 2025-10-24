3598c4af82399296424ec7c7afc8d03e1cfc2ebf
Sumin <new.sumyang@gmail.com>
2025-10-25 01:17:22 +0900

    refactor(RacingCarForwardStrategy): NumberGenerator 로직 수정

--- src/main/java/racingcar/application/numbergenerator/RacingCarForwardStrategy.java
+++ src/main/java/racingcar/application/numbergenerator/RacingCarForwardStrategy.java
@@ -1,14 +1,20 @@
 package racingcar.application.numbergenerator;
 
 import racingcar.application.ForwardStrategy;
-import racingcar.util.RandomNumberGenerator;
+import racingcar.application.NumberGenerator;
 
 public class RacingCarForwardStrategy implements ForwardStrategy {
+
+    private final NumberGenerator numberGenerator;
+
+    public RacingCarForwardStrategy(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
 
     private static final int MOVING_NUMBER = 4;
 
     @Override
     public boolean isMovingForward() {
-        return RandomNumberGenerator.generateRandomNumber() >= MOVING_NUMBER;
+        return numberGenerator.generateNumber() >= MOVING_NUMBER;
     }
 }
