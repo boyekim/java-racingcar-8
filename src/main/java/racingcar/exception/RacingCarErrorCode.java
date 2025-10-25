e92f9c45363275f95a3b8e91491cb747dd5a31af
Sumin <new.sumyang@gmail.com>
2025-10-25 20:40:16 +0900

    refactor(RacingCarErrorCode): 게임 횟수 숫자 예외 메시지 수정

--- src/main/java/racingcar/exception/RacingCarErrorCode.java
+++ src/main/java/racingcar/exception/RacingCarErrorCode.java
@@ -3,7 +3,7 @@ package racingcar.exception;
 public enum RacingCarErrorCode {
     NAME_LENGTH_LIMIT_EXCEEDED("이름은 %s자를 초과할 수 없습니다."),
     NAME_CAN_NOT_BE_EMPTY("이름은 비어있을 수 없습니다."),
-    INVALID_GAME_NUMBER("게임 횟수는 숫자가 입력되어야 합니다."),
+    INVALID_GAME_NUMBER("게임 횟수는 정수가 입력되어야 합니다."),
     GAME_NUMBER_REQUIRED("게임 횟수가 입력되어야 합니다."),
     NEGATIVE_GAME_COUNT_NOT_ALLOWED("게임 횟수는 음수가 입력될 수 없습니다."),
     NAME_REQUIRED("최소 한명의 이름이 입력되어야 합니다."),
