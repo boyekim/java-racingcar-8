58eb009bff361f51b550b342c09c5b7949208f89
Sumin <new.sumyang@gmail.com>
2025-10-25 20:39:42 +0900

    refactor(GameCount): 게임 횟수 음수 예외 처리 추가 및 예외 처리 메서드 분리

--- src/main/java/racingcar/domain/GameCount.java
+++ src/main/java/racingcar/domain/GameCount.java
@@ -11,13 +11,29 @@ public class GameCount {
     }
 
     public static GameCount of(String gameNumber) {
+        validateGameNumberExist(gameNumber);
+        Integer gameCount = validateGameCountInteger(gameNumber);
+        validateGameCountPositive(gameCount);
+        return new GameCount(gameCount);
+    }
+
+    private static void validateGameNumberExist(String gameNumber) {
         if (gameNumber.isEmpty()) {
             throw new RacingCarException(RacingCarErrorCode.GAME_NUMBER_REQUIRED);
         }
+    }
+
+    private static Integer validateGameCountInteger(String gameNumber) {
         try {
-            return new GameCount(Integer.parseInt(gameNumber));
+            return Integer.parseInt(gameNumber);
         } catch (NumberFormatException e) {
             throw new RacingCarException(RacingCarErrorCode.INVALID_GAME_NUMBER);
+        }
+    }
+
+    private static void validateGameCountPositive(Integer gameCount) {
+        if (gameCount < 0) {
+            throw new RacingCarException(RacingCarErrorCode.NEGATIVE_GAME_COUNT_NOT_ALLOWED);
         }
     }
 
--- src/main/java/racingcar/exception/RacingCarErrorCode.java
+++ src/main/java/racingcar/exception/RacingCarErrorCode.java
@@ -5,6 +5,7 @@ public enum RacingCarErrorCode {
     NAME_CAN_NOT_BE_EMPTY("이름은 비어있을 수 없습니다."),
     INVALID_GAME_NUMBER("게임 횟수는 숫자가 입력되어야 합니다."),
     GAME_NUMBER_REQUIRED("게임 횟수가 입력되어야 합니다."),
+    NEGATIVE_GAME_COUNT_NOT_ALLOWED("게임 횟수는 음수가 입력될 수 없습니다."),
     NAME_REQUIRED("최소 한명의 이름이 입력되어야 합니다."),
     DUPLICATED_NAME("이미 존재하는 이름입니다."),
     ;
