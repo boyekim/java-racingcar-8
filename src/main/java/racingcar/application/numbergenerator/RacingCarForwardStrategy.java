47cf50e4a440f0fc3e3f467bdb5354be1e3b59d9
Sumin <new.sumyang@gmail.com>
2025-10-25 21:13:22 +0900

    refactor(RacingCarForwardStrategy): 라인 순서 수정

--- src/main/java/racingcar/application/numbergenerator/RacingCarForwardStrategy.java
+++ src/main/java/racingcar/application/numbergenerator/RacingCarForwardStrategy.java
@@ -7,11 +7,11 @@ public class RacingCarForwardStrategy implements ForwardStrategy {
 
     private final NumberGenerator numberGenerator;
 
+    private static final int MOVING_NUMBER = 4;
+
     public RacingCarForwardStrategy(NumberGenerator numberGenerator) {
         this.numberGenerator = numberGenerator;
     }
-
-    private static final int MOVING_NUMBER = 4;
 
     @Override
     public boolean isMovingForward() {
